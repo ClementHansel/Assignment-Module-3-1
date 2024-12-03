fbfece92ba4620834a48ac0ed9a355c48758bb7b
ClementHansel <greedybugz@gmail.com>
2024-12-03 16:09:33 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 const recipe = {
   title: "Spaghetti Carbonara",
   instructions: [
-    "Cook spaghetti according to package instructions.",
-    "In a separate pan, cook pancetta until crispy.",
-    "In a bowl, whisk eggs and grated cheese.",
-    "Combine spaghetti, pancetta, and egg mixture, stirring quickly.",
-    "Serve immediately with additional cheese and black pepper.",
+    "1. Cook spaghetti according to package instructions.",
+    "2. In a separate pan, cook pancetta until crispy.",
+    "3. In a bowl, whisk eggs and grated cheese.",
+    "4. Combine spaghetti, pancetta, and egg mixture, stirring quickly.",
+    "5. Serve immediately with additional cheese and black pepper.",
   ],
   image: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
 };
@@ -21,17 +21,20 @@ function onRandomRecipe() {
 
   recipeInstruction.innerHTML = "";
 
-  const ol = document.createElement("ol");
+  const ul = document.createElement("ul");
+
+  ul.style.paddingLeft = "0";
+  ul.style.listStyleType = "none";
 
   recipe.instructions.forEach((instruction) => {
     const li = document.createElement("li");
     li.textContent = instruction;
-    ol.appendChild(li);
+    li.style.marginBottom = "10px";
+    ul.appendChild(li);
   });
 
-  recipeInstruction.appendChild(ol);
+  recipeInstruction.appendChild(ul);
 
-  // Set the recipe image
   recipeImage.src = recipe.image;
 
   recipeImage.style.width = "200px";
